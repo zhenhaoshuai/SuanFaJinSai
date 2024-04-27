394ea1d24bcacecd3bf1521c8029eef2fa1605c6
zhenhaoshuai <88430983+zhenhaoshuai@users.noreply.github.com>
2024-04-27 17:22:16 +0800

    Update 分治--快速幂.cpp

--- 分治--快速幂.cpp
+++ 分治--快速幂.cpp
@@ -10,15 +10,20 @@ int n,m;//n等于几次幂，m是底数
 //            2  2  2 2
 //          11 11  11 11
 
+typedef long long ll;
+
 long long solve(int n,int val)
 {
     if (1 == n) {
         return val;
     }
 
-    long long res = solve((n/2),val) * solve((n/2),val);
+    ll temp = solve((n/2),val);
+    long long res = temp * temp;
 
-    if (0 != (n % 2)) { //说明n次幂不是2次倍数，我们提出来一次
+
+//    if (0 != (n % 2)) { //说明n次幂不是2次倍数，我们提出来一次
+    if (1 == n&1){ //这样说明是奇数个
         res *= val;
     }
 
@@ -34,4 +39,23 @@ int main(){
 
     std::printf("%d", result);
     return 0;
+}
+
+
+//------------------------------------书
+
+#include<bits/stdc++.h>
+using namespace std;
+typedef long long ll;                  //注意要用long long，用int会出错
+ll fastPow(ll a, ll n,ll m){           //an mod m
+   if(n == 0)   return 1;             //特判 a0 = 1
+   if(n == 1)   return a;
+   ll temp = fastPow(a, n/2,m);     //分治
+   if(n%2 == 1) return temp * temp * a % m; //奇数个a。也可以这样写： if(n &1)        
+   else    return temp * temp % m ;        //偶数个a        
+}
+int main(){
+   ll a,n,m;   cin >> a >> n>> m;
+   cout << fastPow(a,n,m);
+   return 0;
 }
