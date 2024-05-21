4bf874ac2b943a1cb544ee24e74953c4438fc760
zhenhaoshuai <88430983+zhenhaoshuai@users.noreply.github.com>
2024-05-21 13:12:55 +0800

    Update 二叉树--树型dp(沾边)--验证搜索二叉树.cpp

--- 二叉树--树型dp(沾边)--验证搜索二叉树.cpp
+++ 二叉树--树型dp(沾边)--验证搜索二叉树.cpp
@@ -1,1 +1,72 @@
 
+// 搜索二叉树： 就是  左子树（小于这个节点的值）--- 当前节点 ---右子树（大于这个节点的值）
+
+// 判断是不是搜索二叉树： 可以根据它的中序遍历是不是 升序 的，不是搜索二叉树它就一定不满足这个性质
+//用递归 当然 还有别的方法判断--用 全局变量 lmin lmax rmin rmax 当到底的时候 ，lmax< root <lmin , 然后回到上一级，把上一个方节点的值作为当前的最大值 然后再这么做
+
+
+// 验证搜索二叉树
+// 测试链接 : https://leetcode.cn/problems/validate-binary-search-tree/
+public class Code05_ValidateBinarySearchTree {
+
+	// 不提交这个类
+	public static class TreeNode {
+		public int val;
+		public TreeNode left;
+		public TreeNode right;
+	}
+
+	// 提交以下的方法
+	public static int MAXN = 10001;
+
+	public static TreeNode[] stack = new TreeNode[MAXN];
+
+	public static int r;
+
+	// 提交时改名为isValidBST
+	public static boolean isValidBST1(TreeNode head) {
+		if (head == null) {
+			return true;
+		}
+		TreeNode pre = null;
+		r = 0;
+		while (r > 0 || head != null) {
+			if (head != null) {
+				stack[r++] = head;
+				head = head.left;
+			} else {
+				head = stack[--r];
+				if (pre != null && pre.val >= head.val) {
+					return false;
+				}
+				pre = head;
+				head = head.right;
+			}
+		}
+		return true;
+	}
+
+	public static long min, max;
+
+	// 提交时改名为isValidBST
+	public static boolean isValidBST2(TreeNode head) {
+		if (head == null) {
+			min = Long.MAX_VALUE;
+			max = Long.MIN_VALUE;
+			return true;
+		}
+		boolean lok = isValidBST2(head.left);
+		long lmin = min;
+		long lmax = max;
+		boolean rok = isValidBST2(head.right);
+		long rmin = min;
+		long rmax = max;
+		min = Math.min(Math.min(lmin, rmin), head.val);
+		max = Math.max(Math.max(lmax, rmax), head.val);
+		return lok && rok && lmax < head.val && head.val < rmin;
+	}
+
+}
+
+
+
